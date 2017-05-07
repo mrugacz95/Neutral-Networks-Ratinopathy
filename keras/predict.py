75c1da7290153870fabc9c9bb0fa7e3ccf0c4358
Marcin Mrugas <mrugacz95@gmail.com>
2017-05-07 19:51:08 +0200

    Keras dataset test

--- keras/predict.py
+++ keras/predict.py
@@ -1,13 +1,11 @@
 import random
-
-from keras.datasets import mnist
+import numpy as np
 from keras.models import model_from_json
-import keras
 import matplotlib.pyplot as plt
-
+from load_data import load_retinopathy_data
 
 def main():
-    (x_train, y_train), (X_test, y_test) = mnist.load_data()
+    (x_train, y_train), (X_test, y_test), num_pixels = load_retinopathy_data()  # mnist.load_data()
     json_file = open('model.json', 'r')
     loaded_model_json = json_file.read()
     json_file.close()
@@ -19,17 +17,18 @@ def main():
     # evaluate loaded model on test data
     loaded_model.compile(loss='binary_crossentropy', optimizer='rmsprop', metrics=['accuracy'])
 
-    num_pixels = X_test.shape[1] * X_test.shape[2]
+    # num_pixels = X_test.shape[1] * X_test.shape[2]
     print(X_test.shape)
     number_of_test = 10
     random_offset = random.randint(0, X_test.shape[0] - number_of_test)
     for i in range(random_offset, random_offset + number_of_test):
-        input = X_test[i:i + 1]
-        print(input.shape)
-        plt.imshow(input[0], cmap='gray')
-        input = input.reshape(1, num_pixels).astype('float32')
-        result = loaded_model.predict(input, verbose=1)
-        print(result, y_test[i])
+        input_data = X_test[i:i + 1]
+        img = input_data[0].copy()
+        img = np.reshape(img, (28, 28))
+        plt.imshow(img, cmap='gray')
+        # input = input.reshape(1, num_pixels).astype('float32')
+        result = loaded_model.predict_proba(input_data, verbose=1)
+        print(result, 'expected:', y_test[i])
         plt.show()
 
 
---
+++ load_data.py
@@ -0,0 +1,33 @@
+import glob
+from os.path import basename, splitext
+
+import cv2
+import numpy as np
+
+ratio_taining_to_test = 0.8
+image_size = 28
+
+
+def load_retinopathy_data():
+    num_pixels = image_size * image_size
+    all_images = glob.glob('test_files/cropped_images/*.jpg')[:2500]
+    data = np.empty((len(all_images), image_size * image_size))
+    results = np.empty(len(all_images))
+    for idx, file_path in enumerate(all_images):
+        file_name = basename(file_path)
+        num, score = file_name.replace(r'.jpg', '').split('_')
+        score = round(float(score))
+        img = cv2.imread(file_path, cv2.IMREAD_GRAYSCALE)
+        img = np.reshape(img, num_pixels).astype('float32')
+        data[idx] = img
+        results[idx] = score
+    num_train = round(len(data) * ratio_taining_to_test)
+    num_test = len(data) - num_train
+    X_train, X_test = np.split(data, [num_train])
+    y_train, y_test = np.split(results, [num_train])
+    print('Loaded data')
+    return (X_train, y_train), (X_test, y_test), num_pixels
+
+
+if __name__ == '__main__':
+    load_retinopathy_data()
