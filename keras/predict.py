9acd9d306726a40d4ce226e8945e77ec8c82d72f
Marcin Mrugas <mrugacz95@gmail.com>
2017-05-07 23:50:37 +0200

    Improved input processing on gpu

--- keras/predict.py
+++ keras/predict.py
@@ -12,7 +12,7 @@ from load_data import load_retinopathy_data
 
 
 def predict(input_data, loaded_model):
-    return loaded_model.predict(input_data, verbose=0)
+    return loaded_model.predict(input_data, verbose=1)
 
 
 def load_model():
@@ -24,26 +24,33 @@ def load_model():
     loaded_model.load_weights("model.h5")
     # evaluate loaded model on test data
     loaded_model.compile(loss='binary_crossentropy', optimizer='rmsprop', metrics=['accuracy'])
+    print("Loaded model from disk")
     return loaded_model
 
 
 def processImage(filepath: str, output: str):
     loaded_model = load_model()
     image = cv2.imread(filepath, cv2.IMREAD_GRAYSCALE)
-    image = image[400:600, 400:600]
-    cv2.imshow('input',image)
+    #image = image[400:600, 400:600]
     cv2.imwrite(output + 'source.jpg', image)
-    result = np.zeros((image.shape[0], image.shape[1]),np.uint8)
-    for x in range(28, image.shape[0] - 28):
-        for y in range(28, image.shape[1] - 28):
+    result = np.zeros((image.shape[0], image.shape[1]), np.uint8)
+    w, h = image.shape
+    output_shape = (w - 28) * (h - 28)
+    network_input = np.empty(((w - 28) * (h - 28), 28 * 28))
+    index = 0
+    for x in range(0, image.shape[0] - 28):
+        for y in range(0, image.shape[1] - 28):
             cropped_img = image[x:x + 28, y:y + 28]
             cropped_img = np.reshape(cropped_img, (28 * 28))
-            value = predict(np.array([cropped_img, ]), loaded_model)
-            value *= 255
-            print(x, y, 'value', value)
-            result[x][y] = value
-    cv2.imwrite(output, result)
-    cv2.imshow('result', result)
+            network_input[index] = cropped_img
+            if index % 1000 == 0:
+                print(index, ' out of ', network_input.shape[1])
+            index += 1
+    values = predict(network_input, loaded_model)
+    values *= 255
+    values = np.reshape(values, (w - 28, h - 28))
+    cv2.imwrite(output, values)
+    cv2.imshow('result', values)
     cv2.waitKey()
     return result
 
@@ -51,7 +58,6 @@ def processImage(filepath: str, output: str):
 def main():
     (x_train, y_train), (X_test, y_test), num_pixels = load_retinopathy_data()  # mnist.load_data()
     loaded_model = load_model()
-    print("Loaded model from disk")
 
     # num_pixels = X_test.shape[1] * X_test.shape[2]
     print(X_test.shape)
@@ -69,6 +75,7 @@ def main():
 
 
 if __name__ == '__main__':
+    print('Loaded tesor flow')
     test_file_path = glob.glob('test_files/full_images/*.jpg')[0]
     test_file_name = splitext(basename(test_file_path))[0]
-    result = processImage(test_file_path, './test_files/results_images/' + test_file_name + '.jpg')
+    result = processImage(test_file_path, 'test_files/output/' + test_file_name + '.jpg')
